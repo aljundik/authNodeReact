b8dceee93559b0433e6e4c94d9a4a5ffb668eb8b
Khaled Aljundi <aljundik@gmail.com>
2017-07-16 02:39:51 +0300

    add local startegy passport

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,24 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+//create local strategy
+const localOptions ={usernameField: 'email'}
+const localLogin = new LocalStrategy(localOptions,function(email,passoword,done){
+    //verify this username and password, call done with the user
+    //if it correct email and password
+    //otherwise, call done with false
+    User.findOne({email: email}, function(err,user){
+        if (err){return done(err);}
+        if(!user){return done(null,false);}
+
+        //compare passwords = is 'password' equal to user.password
+        
+    })
+
+
+})
 
 //setup options for JWT Strategy
 const jwtOptions={
@@ -33,6 +50,8 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload,done){
         
     })
 });;
+
+
 
 
 //tell passport to use this strategy
